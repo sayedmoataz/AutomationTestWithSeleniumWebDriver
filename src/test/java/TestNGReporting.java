d3e560a0aafb359c9680245636c28d3fee95977f
sayedmoataz <sayedmoataz9@gmail.com>
2025-01-13 02:01:56 +0200

    using assertation to verify test scenario hard assertation passed soft assertation failed

--- src/test/java/TestNGReporting.java
+++ src/test/java/TestNGReporting.java
@@ -3,17 +3,24 @@ import org.metachain.drivers.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
 
 public class TestNGReporting {
     private static final String browserType = "chrome";
     private static final String WEBSITE_URL = "https://appleslice.vercel.app";
-    private static final String USERNAME = "S@yedMoataz";
-    private static final String USER_EMAIL = "sayedmoataz9@gmail.com";
+    private static final String FIRST_NAME = "S@yed";
+    private static final String LAST_NAME = "S@yed";
+    private static final String USER_EMAIL = "satz9@gmail.com";
     private static final String PASSWORD = "sayed123456";
-    private static final String USER_NUMBER = "01147880178";
+    private static final String USER_NUMBER = "01111001122";
     private static final int COUNTRY_NUMBER_ID = 56;
 
     private WebDriver driver;
@@ -30,15 +37,36 @@ public class TestNGReporting {
         clickElement(By.cssSelector("#screen > div:nth-child(4) > button"));  // Get Started
         clickElement(By.cssSelector("#screen > main > div:nth-child(6) > p:nth-child(2)"));  // Sign Up
 
+        // Get the form fields before filling them
+        WebElement firstNameField = driver.findElement(By.xpath("//*[@id=\"screen\"]/main/div[1]/input"));
+        WebElement lastNameField = driver.findElement(By.xpath("//*[@id=\"screen\"]/main/div[2]/input"));
+
+        // Fill the form
         fillRegistrationForm();
 
+        // test optional cases
+        SoftAssert softAssert = new SoftAssert();
+        String firstName = firstNameField.getAttribute("value");
+        String lastName = lastNameField.getAttribute("value");
+
+        softAssert.assertNotEquals(firstName.trim(), lastName.trim(), "First name and last name should not be identical");
+
         clickElement(By.xpath("//*[@id=\"screen\"]/main/div[7]/button"));  // Sign Up Button
-        Thread.sleep(10000);
+
+        // test required case
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement popupMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".styleToast")));
+        String expectedMessage = "User registered successfully";
+
+
+        Assert.assertEquals(popupMessage.getText(), expectedMessage, "Registration success message mismatch!");
+
+        softAssert.assertAll();
     }
 
     private void fillRegistrationForm() {
-        sendKeysToElement(By.xpath("//*[@id=\"screen\"]/main/div[1]/input"), USERNAME);  // First Name
-        sendKeysToElement(By.xpath("//*[@id=\"screen\"]/main/div[2]/input"), USERNAME);  // Last Name
+        sendKeysToElement(By.xpath("//*[@id=\"screen\"]/main/div[1]/input"), FIRST_NAME);  // First Name
+        sendKeysToElement(By.xpath("//*[@id=\"screen\"]/main/div[2]/input"), LAST_NAME);  // Last Name
         sendKeysToElement(By.xpath("//*[@id=\"screen\"]/main/div[3]/input"), USER_EMAIL);  // Email
 
         selectCountry();
